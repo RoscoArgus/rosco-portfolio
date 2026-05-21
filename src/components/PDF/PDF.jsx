a69726eef92debb49ede735d9d74bec332b08cac
Ross Devine <roscod@live.ie>
2026-05-21 15:44:08 +0100

    Add slight bottom margin to PDF

--- src/components/PDF/PDF.jsx
+++ src/components/PDF/PDF.jsx
@@ -19,6 +19,7 @@ export const PDF = ({ src, title }) => {
           width: "60%",
           minWidth: "750px",
           height: "100%",
+          marginBottom: "30px",
         }}
       />
     </div>
