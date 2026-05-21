463287c30420ceeed7645959b5f6b9ca67bf8b62
Ross Devine <roscod@live.ie>
2026-05-21 15:42:58 +0100

    Update PDF width

--- src/components/PDF/PDF.jsx
+++ src/components/PDF/PDF.jsx
@@ -1,13 +1,26 @@
-import { Navbar } from '../Navbar/Navbar';
+import { Navbar } from "../Navbar/Navbar";
 
 export const PDF = ({ src, title }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', alignItems: 'center' }}>
-       <iframe
-            src={src}
-            title={title}
-            style={{ border: '5px solid white', borderRadius: '10px', width: '45%', height: '100%'}}
-        /> 
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        alignItems: "center",
+      }}
+    >
+      <iframe
+        src={src}
+        title={title}
+        style={{
+          border: "5px solid white",
+          borderRadius: "10px",
+          width: "60%",
+          minWidth: "750px",
+          height: "100%",
+        }}
+      />
     </div>
   );
 };
