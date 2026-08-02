4124677528cb2a201739f0e527709f5e54b405cb
Ross Devine <roscod@live.ie>
2026-08-02 20:26:19 +0100

    Add titles to Carousel elements

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -14,8 +14,10 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
   return (
     <ul className="carousel" style={{ '--n': items.length } as CSSProperties}>
       {items.map((item, index) => (
-        <li key={index} title={item.title} style={{ '--i': index } as CSSProperties}>
-          <img src={item.icon} alt={item.title} />
+        <li key={index} title={item.title} style={{ '--i': index, '--title': item.title } as CSSProperties}>
+          <div className="carouselImageContainer">
+            <img src={item.icon} alt={item.title} />
+          </div>
         </li>
       ))}
     </ul>
