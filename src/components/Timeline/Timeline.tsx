4d4fe97c1114ad0fae98b60ea623e12637045137
Ross Devine <roscod@live.ie>
2026-08-10 15:26:11 +0100

    Initial implementation of timeline component for work experience

---
+++ src/components/Timeline/Timeline.tsx
@@ -0,0 +1,59 @@
+import './Timeline.css';
+import { useState, useRef } from 'react';
+
+interface TimelineProps {
+  experiences: Experience[];
+}
+
+interface Role {
+  title: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+}
+
+interface Experience {
+  title: string;
+  roles: Role[];
+  icon: string;
+  banner: string;
+  backgroundColor: string;
+}
+
+const Timeline: React.FC<TimelineProps> = ({ experiences }) => {
+  const [activeExperience, setActiveExperience] = useState<Experience | null>(null);
+
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = (experience: Experience) => {
+    setActiveExperience(experience);
+
+    requestAnimationFrame(() => {
+      popoverRef.current?.showPopover();
+    });
+  };
+
+  return (
+    <div className="timeline">
+      {experiences.map((experience, index) => (
+        <button key={index} type="button" className="timeline-item" onClick={() => handleClick(experience)}>
+          <img className="timeline-icon" src={experience.icon} alt={experience.title} />
+        </button>
+      ))}
+      {activeExperience && (
+        <div
+          ref={popoverRef}
+          id="experience-popover"
+          popover="auto"
+          className="experience-popover"
+          style={{ backgroundColor: activeExperience.backgroundColor }}
+        >
+          <img className="experience-banner" src={activeExperience.banner} alt={activeExperience.title} />
+          {activeExperience.title}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Timeline;
--- src/sections/Experience/Experience.tsx
+++ src/sections/Experience/Experience.tsx
@@ -1,9 +1,62 @@
+import Timeline from '../../components/Timeline/Timeline';
 import './Experience.css';
+import workdayIcon from '../../assets/experience/workday-icon.png';
+import workdayBanner from '../../assets/experience/workday-banner.png';
+import nitroIcon from '../../assets/experience/nitro-icon.png';
+import nitroBanner from '../../assets/experience/nitro-banner.png';
+import quantexaIcon from '../../assets/experience/quantexa-icon.png';
+import quantexaBanner from '../../assets/experience/quantexa-banner.png';
 
 const Experience = () => {
+  const experiences = [
+    {
+      title: 'Nitro',
+      roles: [
+        {
+          title: 'Student Software Engineer',
+          startDate: new Date('2023-01-24'),
+          endDate: new Date('2023-04-04'),
+          description: 'TODO',
+        },
+      ],
+      icon: nitroIcon,
+      banner: nitroBanner,
+      backgroundColor: '#FC6030',
+    },
+    {
+      title: 'Quantexa',
+      roles: [
+        {
+          title: 'Student Project Lead',
+          startDate: new Date('2024-01-23'),
+          endDate: new Date('2024-04-9'),
+          description: 'TODO',
+        },
+      ],
+      icon: quantexaIcon,
+      banner: quantexaBanner,
+      backgroundColor: '#342243',
+    },
+    {
+      title: 'Workday',
+      roles: [
+        {
+          title: 'Software Development Engineer Intern',
+          startDate: new Date('2025-02-10'),
+          endDate: new Date('2025-08-22'),
+          description: 'TODO',
+        },
+      ],
+      icon: workdayIcon,
+      banner: workdayBanner,
+      backgroundColor: '#0F2E66',
+    },
+  ];
+
   return (
     <section className="experience" id="experience">
       <h1>Experience</h1>
+      <Timeline experiences={experiences} />
     </section>
   );
 };
