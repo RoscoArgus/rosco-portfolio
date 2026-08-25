f01dc7f74a11809891a02412b2db1a09a7119c6e
Ross Devine <roscod@live.ie>
2026-08-25 22:01:34 +0100

    Apply traverable styling at all times and add "in progress" cards on either side of ProjectCards

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -2,13 +2,13 @@ import ProjectCard from '../../components/ProjectCard/ProjectCard';
 import './Projects.css';
 import projects from './data/projects';
 import { useState, type CSSProperties } from 'react';
-import { FaAngleLeft, FaAngleDoubleLeft, FaAngleRight, FaAngleDoubleRight } from 'react-icons/fa';
+import { FaAngleLeft, FaAngleDoubleLeft, FaAngleRight, FaAngleDoubleRight, FaTools } from 'react-icons/fa';
 
 const Projects = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(1);
 
   const updateIndex = (index: number) => {
-    const newIndex = Math.max(0, Math.min(index, projects.length - 1));
+    const newIndex = Math.max(1, Math.min(index, projects.length));
     setCurrentIndex(newIndex);
   };
 
@@ -16,21 +16,35 @@ const Projects = () => {
     <section className="projects" id="projects">
       <h1>Projects</h1>
       <div className="projects-items" style={{ '--index': currentIndex } as CSSProperties}>
+        <div className="ghost-card">
+          <FaTools className="icon" />
+        </div>
         {projects.map((project) => (
           <ProjectCard project={project} />
         ))}
+        <div className="ghost-card">
+          <FaTools className="icon" />
+        </div>
       </div>
       <span className="projects-controls">
-        <button onClick={() => updateIndex(0)} disabled={currentIndex === 0}>
+        <button onClick={() => updateIndex(0)} disabled={currentIndex === 1} title="Skip to Start">
           <FaAngleDoubleLeft className="icon" />
         </button>
-        <button onClick={() => updateIndex(currentIndex - 1)} disabled={currentIndex === 0}>
+        <button onClick={() => updateIndex(currentIndex - 1)} disabled={currentIndex === 1} title="Previous Project">
           <FaAngleLeft className="icon" />
         </button>
-        <button onClick={() => updateIndex(currentIndex + 1)} disabled={currentIndex === projects.length - 1}>
+        <button
+          onClick={() => updateIndex(currentIndex + 1)}
+          disabled={currentIndex === projects.length}
+          title="Next Project"
+        >
           <FaAngleRight className="icon" />
         </button>
-        <button onClick={() => updateIndex(projects.length - 1)} disabled={currentIndex === projects.length - 1}>
+        <button
+          onClick={() => updateIndex(projects.length)}
+          disabled={currentIndex === projects.length}
+          title="Skip to End"
+        >
           <FaAngleDoubleRight className="icon" />
         </button>
       </span>
